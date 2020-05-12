8995aa5297ba44c816485fdcdb34fb94c8c51774
Christian <carl_hassel@hotmail.com>
2020-05-12 14:01:58 +0200

    Initial commit

---
+++ src/main/java/com/nmr/nmp/NordicMotorhomeRentalsApplication.java
@@ -0,0 +1,13 @@
+package com.nmr.nmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordicMotorhomeRentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordicMotorhomeRentalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nmr/NordicMotorhomeRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordicMotorhomeRentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
