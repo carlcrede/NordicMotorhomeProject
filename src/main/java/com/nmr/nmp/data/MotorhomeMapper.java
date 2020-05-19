4fd1ac99178ac4a483f42b65319ef4c00ef1eb15
Christian <carl_hassel@hotmail.com>
2020-05-19 17:06:33 +0200

    MotorhomeMapper - changes to how statements and resultset are closed

--- src/main/java/com/nmr/nmp/data/MotorhomeMapper.java
+++ src/main/java/com/nmr/nmp/data/MotorhomeMapper.java
@@ -10,26 +10,31 @@ import java.util.ArrayList;
 
 public class MotorhomeMapper {
 
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
     public void create(Motorhome motorhome) {
-        try (Connection connection = DBManager.getConnection()) {
+        try {
+            Connection connection = DBManager.getConnection();
             String sql = "INSERT INTO motorhome (brand, model) VALUES (?, ?)";
-            PreparedStatement ps = connection.prepareStatement(sql);
+            ps = connection.prepareStatement(sql);
             ps.setString(1, motorhome.getBrand());
             ps.setString(2, motorhome.getModel());
             ps.execute();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (ps != null) { try { ps.close(); } catch (SQLException e) { e.printStackTrace(); } }
         }
     }
 
     public ArrayList<Motorhome> read() {
         ArrayList<Motorhome> motorhomes = new ArrayList<>();
+        String sql = "SELECT * FROM motorhome";
         try {
             Connection connection = DBManager.getConnection();
-            String sql = "SELECT * FROM motorhome";
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
             while (rs.next()) {
                 String model = rs.getString("model");
                 String brand = rs.getString("brand");
@@ -37,6 +42,10 @@ public class MotorhomeMapper {
                 motorhomes.add(motorhome);
             }
         } catch (SQLException e) { e.getMessage(); }
+        finally {
+            if (ps != null) { try { ps.close(); } catch (SQLException e) { e.printStackTrace(); } }
+            if (rs != null) { try { rs.close(); } catch (SQLException e) { e.printStackTrace(); } }
+        }
         return motorhomes;
     }
 }
