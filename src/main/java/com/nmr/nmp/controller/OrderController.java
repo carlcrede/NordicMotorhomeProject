99f94f19f84d0aec514fdfad34e6312a8c26f7da
MadGhoulishKunt <iw4ffl3z@gmail.com>
2020-05-28 10:28:06 +0200

    dtjdrtj

--- src/main/java/com/nmr/nmp/controller/OrderController.java
+++ src/main/java/com/nmr/nmp/controller/OrderController.java
@@ -14,9 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 
 @Controller
