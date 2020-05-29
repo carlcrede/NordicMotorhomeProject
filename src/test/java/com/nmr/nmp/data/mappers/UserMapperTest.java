cd7002615d97b4f5154f26500f898e656e00d4c0
MadGhoulishKunt <iw4ffl3z@gmail.com>
2020-05-29 15:16:22 +0200

    Login tests

--- src/test/java/com/nmr/nmp/data/mappers/UserMapperTest.java
+++ src/test/java/com/nmr/nmp/data/mappers/UserMapperTest.java
@@ -1,27 +1,92 @@
 package com.nmr.nmp.data.mappers;
 
 import com.nmr.nmp.data.DBManager;
+import com.nmr.nmp.domain.exceptions.LoginException;
 import com.nmr.nmp.domain.models.User;
 import com.nmr.nmp.utility.PasswordEncoder;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.sql.Connection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserMapperTest {
 
+    UserMapper userMapper;
+    User user;
 
+    @BeforeAll
+    void init(){
+        userMapper = new UserMapper();
+        user = new User();
+    }
 
     @Test
-    void loginTest() {
-        //User user = new User();
+    void loginUserFirstnameTest() {
+        String expected = "John";
 
-        UserMapper um = new UserMapper();
-        User actual = um.login("jd1234", PasswordEncoder.encode("MitKodeord123!"));
-        //User expected = new User("John", "Doe", "Owner","jd1234");
-        assertEquals("John", actual.getFirstName());
+        try {
+            user = userMapper.login("jd1234", PasswordEncoder.encode("MitKodeord123!"));
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+
+        String actual = user.getFirstName();
+
+        assertEquals(expected, actual);
 
 
     }
+
+    @Test
+    void loginUserLastnameTest() {
+        String expected = "Doe";
+
+        try {
+            user = userMapper.login("jd1234", PasswordEncoder.encode("MitKodeord123!"));
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+
+        String actual = user.getLastName();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void loginUserRoleTest(){
+        String expected = "owner";
+
+        try {
+            user = userMapper.login("jd1234", PasswordEncoder.encode("MitKodeord123!"));
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+
+        String actual = user.getRole();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void loginUserUsernameTest(){
+        String expected = "jd1234";
+
+        try {
+            user = userMapper.login("jd1234", PasswordEncoder.encode("MitKodeord123!"));
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+
+        String actual = user.getUserName();
+
+        assertEquals(expected, actual);
+
+    }
+
 }
